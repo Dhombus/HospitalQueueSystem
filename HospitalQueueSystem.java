2d8e4d62493906422e55df0f23d6d40ab5a96e83
Dhombus <diojames908@gmail.com>
2025-01-26 23:50:57 +0800

    Update HospitalQueueSystem.java

--- HospitalQueueSystem.java
+++ HospitalQueueSystem.java
@@ -2,6 +2,7 @@
 
 public class HospitalQueueSystem {
     public static void main(String[] args) throws Exception {
+        // Window size is determined here since layout is not adjusted to be resizable
         int windowWidth = 750;
         int windowHeight = 500;
 
